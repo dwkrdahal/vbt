5a245a16f63023086bc110a7e2c0337a021bdd93
Diwakar <dwkrdahal@gmail.com.com>
2024-06-29 09:45:16 +0545

    minor change

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ import errorMiddleware from "./middlewares/error.middleware.js";
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.use("/api/auth", authRoute);
 app.use("/api/form", contactRoute);
